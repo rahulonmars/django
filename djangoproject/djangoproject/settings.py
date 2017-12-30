d7a98939da54bccfe7d21c35f299593c56555043
rahulonmars <rahulonmars@gmail.com>
2017-12-30 19:09:42 +0530

    created posts app

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -31,12 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
@@ -73,13 +74,23 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoproject',
+        'USER': 'admin',
+        'PASSWORD': '123456',
+        'HOST':'localhost',
+        'PORT': ''
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
---
+++ djangoproject/posts/views.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Posts
+
+# Create your views here.
+
+def index(request):
+
+    posts = Posts.objects.all()[:10]
+
+    context = {
+        'title':'Latest Posts',
+        'posts': posts
+    }
+    #return HttpResponse("Hello From Posts.")
+    return render(request, 'posts/index.html', context)
+
+def details(request, id):
+    post = Posts.objects.get(id=id)
+
+    context = {
+        'post': post
+    }
+
+    return render(request, 'posts/details.html', context)
